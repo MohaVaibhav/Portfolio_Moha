5c5ddf9256cbd06c601a11b24a0f7308b70601cf
vaibhavbagekari <134953230+vaibhavbagekari@users.noreply.github.com>
2024-01-10 18:07:20 +0530

    my portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+function ToggleEvent() {
+    var obj=document.getElementById("navigration");
+    obj.classList.toggle("navigation_mobile");
+    obj.classList.toggle("navigation");
+    var obj2=document.getElementById("overlay");
+    if (obj2.style.display === "none"){
+        obj2.style.display = "block";
+    }
+    else{
+        obj2.style.display = "none";
+    }
+}
